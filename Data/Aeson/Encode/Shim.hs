a917f6a9e91a1c8561337b4675a27cf542774bbf
Robin Green <greenrd@greenrd.org>
2017-05-20 11:08:36 +0100

    Fix deprecation warning

--- Data/Aeson/Encode/Shim.hs
+++ Data/Aeson/Encode/Shim.hs
@@ -72,7 +72,7 @@ import qualified Data.Vector                       as V
 import           Numeric                           (showHex)
 
 #if MIN_VERSION_bytestring(0,10,4)
-import           Data.Aeson.Encode                 (encode)
+import           Data.Aeson                        (encode)
 #else
 import           Data.Aeson.Types                  (ToJSON (toJSON))
 import qualified Data.ByteString.Lazy              as BL
